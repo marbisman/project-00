05cb49b1c3f49c0a97c92c35551c06bdd6a3b664
marbisman <maria.arbisman@gmail.com>
2017-10-04 16:43:15 -0700

    updated psuedo code in script, added jquery onload, setup variable for game

---
+++ js/app.js
@@ -0,0 +1,34 @@
+//add juery on ready
+$(document).ready(function() {
+
+//name all of the variable from html ids
+var score = $('#score');
+var track = $('#track');
+var line1 = $('#line1');
+var line2 = $('#line2');
+var line3 = $('#line3');
+var yourCar = $('#yourCar');
+var car1 = $('#car1');
+var car2 = $('#car2');
+var car3 = $('#car3');
+var restartBtn = $('#restart');
+
+//initial setup of your track
+
+//initial setup of your car
+
+//initial setup of opponent cars
+
+//score
+
+//your car speed
+
+//the line speed
+
+//move your car
+
+//move opponent cars
+
+//move your lines so the track looks like it is moving
+
+//restart button
