b2a627a6ec9cd9060e50d71c2d5e89999312d25c
marbisman <maria.arbisman@gmail.com>
2017-10-10 16:12:32 -0700

    updated javascript layout

--- app.js
+++ app.js
@@ -1,56 +1,21 @@
 //add juery on ready
 $(document).ready(function() {
 
-//name all of the variable from html ids
-var score = $('#score');
-var track = $('#track');
-var line1 = $('#line1');
-var line2 = $('#line2');
-var line3 = $('#line3');
-var yourCar = $('#yourCar');
-var car1 = $('#car1');
-var car2 = $('#car2');
-var car3 = $('#car3');
-var restartBtn = $('#restart');
-
 //initial setup of your track
-var lineSpeed = 3;
+var track= document.getElementById('track');
+// drawing context for canvas
+var canvas = track.getContex("2d");
+var car = canvas.fillRect(10,20,20,40);
+var carO = canvas.fillRect(10,90,20,40);
 
-//initial setup of your car
-var endGame = false;
-var score = 0;
-var speed = 1;
 
+//move car
 
-//initial setup of opponent cars
+//move car opponent
 
-//score
+//pick a winner
 
-//your car speed
+//tie game
 
-//the line speed
-
-//move your car
-$(document).on('keydown', function(input)){
-  if (endGame === false){
-    var key = input.keyCode;
-    if (key ===37 && move_left === false){
-      move_left = requestAnimationFrame(left);
-    }
-    else if (key === 39 && move_right === false){
-      move_right = requestAnimationFrame(right);
-    }
-    else if (key === 38 && move_up === false){
-      move_up = requestAnimationFrame(up);
-    }
-    else if (key === 40 && move_down === false){
-      move_down = requestAnimationFrame(down);
-    }
-  }
+//restart
 });
-
-//move opponent cars
-
-//move your lines so the track looks like it is moving
-
-//restart button
