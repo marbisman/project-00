8e916cc85a203947784d92368baeb1bf2a460bef
marbisman <maria.arbisman@gmail.com>
2017-10-12 11:14:31 -0700

    redecided using Canvas and started over with click events

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
+//global variables
+var endGame = false;
+
 //add juery on ready
 $(document).ready(function() {
+console.log ("ready");
 
-//initial setup of your track
-var track= document.getElementById('track');
-// drawing context for canvas
-var canvas = track.getContex("2d");
-var car = canvas.fillRect(10,20,20,40);
-var carO = canvas.fillRect(10,90,20,40);
-
-
-//move car
-
-//move car opponent
-
+//add players for start
+$(document).on("click",function(){
+  function playersStart(){
+    $("#player1").addClass("p1Ready");
+    $("#player2").addClass("p2Ready");
+    $('button').removeClass("start").addClass("playing");
+  }
+});
+//move car 1 using right arrow key and 1 key
+$(document).on("click",function(){
+      if(key.keyCode === 39 && endGame === false){
+        moveCars("car1");
+      }
+      if(key.keyCode === 49 && endGame === false){
+        moveCars("car2");
+      }
+    });
+  });
 //pick a winner
 
 //tie game
 
 //restart
-});
